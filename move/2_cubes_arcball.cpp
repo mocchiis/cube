6c09f203566e9f5f41e5702f5ecfc33346e4e232
mocchiis <45111449+mocchiis@users.noreply.github.com>
2019-06-27 20:45:19 +0900

    Update 2_cubes_arcball.cpp

--- move/2_cubes_arcball.cpp
+++ move/2_cubes_arcball.cpp
@@ -20,7 +20,7 @@
 
 static int WIN_WIDTH = 500;                       // ウィンドウの幅
 static int WIN_HEIGHT = 500;                       // ウィンドウの高さ
-static const char *WIN_TITLE = "";     // ウィンドウのタイトル
+static const char *WIN_TITLE = "moving_cubes";     // ウィンドウのタイトル
 
 // シェーダファイル
 static std::string VERT_SHADER_FILE = std::string(SHADER_DIRECTORY) + "render.vert";
